e428a7470073127b167809efa1fb024aae2b92b4
mashakrot <makrosensei@gmail.com>
2025-06-13 19:50:05 +0300

    sentry

---
+++ sentry.client.config.js
@@ -0,0 +1,11 @@
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://5b307e5a486420d77bcf98f1fe65ccaa@o4509481387753472.ingest.de.sentry.io/4509481407807568",
+  integrations: [
+    Sentry.feedbackIntegration({
+      // Additional SDK configuration goes in here, for example:
+      colorScheme: "system",
+    }),
+  ],
+});
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://5b307e5a486420d77bcf98f1fe65ccaa@o4509481387753472.ingest.de.sentry.io/4509481407807568",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
